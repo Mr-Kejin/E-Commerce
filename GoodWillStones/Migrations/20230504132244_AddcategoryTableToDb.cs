e0b950fa2824325cf1dad990e74406530f1b6e9b
Kejin_Anit <kejin.ajit@micrologic.net>
2023-05-04 22:31:27 +0530

    Migration files (need to double check it )

---
+++ GoodWillStones/Migrations/20230504132244_AddcategoryTableToDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoodWillStones.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddcategoryTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    lCategory_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    sDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    sDisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.lCategory_ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ GoodWillStones/Migrations/20230504163552_seedcategrytable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GoodWillStones.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedcategrytable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "lCategory_ID", "sDescription", "sDisplayOrder" },
+                values: new object[,]
+                {
+                    { 1, "Brick", 1 },
+                    { 2, "M-Sand", 2 },
+                    { 3, "P-Sand", 3 },
+                    { 4, "Blue-Metal", 4 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "lCategory_ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "lCategory_ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "lCategory_ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "lCategory_ID",
+                keyValue: 4);
+        }
+    }
+}
