a1796b8e040dc454d2dee35f0897ac72c7b0a216
Kejin_Anit <kejin.ajit@micrologic.net>
2023-05-22 22:46:03 +0530

    BUYBOX-4 we have created the CRUD operation for the Categories and added the toster for notification

--- GoodWillStones/Controllers/CategoryController.cs
+++ GoodWillStones/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using GoodWillStones.Data;
 using GoodWillStones.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GoodWillStones.Controllers
 {
@@ -8,7 +9,7 @@ namespace GoodWillStones.Controllers
     {
         private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext db) 
+        public CategoryController(ApplicationDbContext db)
         {
             _db = db;
         }
@@ -23,19 +24,74 @@ namespace GoodWillStones.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult CreateCategory (Category Obj)
+        public IActionResult CreateCategory(Category Obj)
         {
-            if (Obj.sDescription == Obj.sDisplayOrder.ToString())
-            {
-                ModelState.AddModelError("sDescription", "Display odder and name cant have same data.");
-            }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(Obj); // we are telling the entity fw to add this category to the entity table 
                 _db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
             return View();
         }
+        public IActionResult EditCategory(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0)
+            {
+                return NotFound();
+            }
+            // Category CategoryFromDb = _db.Categories.Find(categoryId); find will ony work on the primary key of the table. but first or default will work everytime 
+            Category? CategoryFromDb = _db.Categories.FirstOrDefault(u => u.lCategory_ID == categoryId);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult EditCategory(Category Obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Obj); // we are telling the entity fw to add this category to the entity table 
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        // Delete Category
+        // view 
+        public IActionResult DeleteCategory(int? categoryId) // since the parameters are same we cant use the same delete name below
+        {
+            if (categoryId == null || categoryId == 0)
+            {
+                return NotFound();
+            }
+            // Category CategoryFromDb = _db.Categories.Find(categoryId); find will ony work on the primary key of the table. but first or default will work everytime 
+            Category? CategoryFromDb = _db.Categories.FirstOrDefault(u => u.lCategory_ID == categoryId);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        //delete Action
+        [HttpPost, ActionName ("DeleteCategory")]
+        public IActionResult DeleteCategoryPost (int? categoryId)
+        {
+            Category? obj = _db.Categories.Find(categoryId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            TempData["Success"] = "Category Deleted Successfully";
+            _db.SaveChanges();
+            return RedirectToAction("Index");         
+        }
+
     }
 }
