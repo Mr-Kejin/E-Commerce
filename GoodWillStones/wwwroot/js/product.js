0b4eba937157e96140da0a5be8419cc079019f4e
Kejin_Anit <kejin.ajit@micrologic.net>
2023-12-30 01:53:56 +0530

    working on the datatable edit function

--- GoodWillStones/wwwroot/js/product.js
+++ GoodWillStones/wwwroot/js/product.js
@@ -9,7 +9,19 @@ function loadDataTable() {
             { data: 'sProductName', "width": "15%" },
             { data: 'listPrice', "width": "15%" },
             { data: 'category.sDescription', "width": "15%" },
+            {
+                data: 'Product_ID',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/Admin/Product/UpsertProduct?Product_ID=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square" ></i > EDIT </a>
+                    <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill" ></i > Delete</a>
+                    </div>`               
+                },
+                "width": "15%"
+            }
             
+
+
         ]
     });
 }
